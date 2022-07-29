f14e56ae72f61a86d5b92f0faeaa93d37a780a17
Caroline-githubb <ingrid_caroliine@yahoo.com.br>
2022-07-29 09:22:38 -0300

    Aulas 83 a 93

---
+++ CursoNelio/Ex06/Program.cs
@@ -0,0 +1,270 @@
+﻿
+//SWITCH CASE
+// int x = int.Parse(Console.ReadLine());
+// string day;
+
+// switch (x)
+// {
+//     case 1:
+//         day = "Sunday";
+//         break;
+//     case 2:
+//         day = "Monday";
+//         break;
+//     case 3:
+//         day = "Tuesday";
+//         break;
+//     case 4:
+//         day = "Wednesday";
+//         break;
+//     case 5:
+//         day = "Thursday";
+//         break;
+//     case 6:
+//         day = "Friday";
+//         break;
+//     case 7:
+//         day = "Saturday";
+//         break;
+//     default:
+//         day = "Invalid value";
+//         break;
+// }
+
+// Console.WriteLine("Day: " + day);
+
+
+//CONDICAO TERNARIA
+// using System.Globalization;
+
+// double preco = double.Parse(Console.ReadLine()+"", CultureInfo.InvariantCulture);
+// double desconto = (preco < 20.0) ? preco*0.1 : preco * 0.05;
+
+// // if(preco<20.0)
+// // {
+// //     desconto = preco*0.1;
+// // }
+// // else
+// // {
+// //     desconto = preco*0.05;
+// // }
+
+
+// Console.WriteLine("Desconto: "+desconto);
+
+
+// //FUNCOES INTERESSANTES PARA STRING
+// string original = "abcde FGHI ABC abc DEFG";
+
+// //Deixa tudo com letra maiuscula
+// string s1 = original.ToUpper();
+// //Deixa tudo com letra minuscula
+// string s2 = original.ToLower();
+// //voltou para a forma original
+// string s3 = original.Trim();
+// //Encontra a primeira posicao onde tem bc
+// int n1 = original.IndexOf("bc");
+// //Encontra a ultima posicao onde tem bc
+// int n2 = original.LastIndexOf("bc");
+// //Corta as letras partir da posicao que escolher ficando(nesse exemplo, as letras apareceram a partir do "de" ate o final) 
+// string s4 = original.Substring(3);
+// ///Corta as letras partir da posicao que escolher e vai ate a posicao que quiser(nesse exemplo as letras apareceram do "de" ate o "FG)
+// string s5 = original.Substring(3, 5);
+// //Substitui as palavras e nao precisam serem de mesmo tamanho
+// string s6 = original.Replace('a', 'x');
+// string s7 = original.Replace("abc", "xy");
+
+// //testa se a string é nula ou vazia
+// bool b1 = String.IsNullOrEmpty(original);
+// //testa se é nula ou tem espaco em branco
+// bool b2 = String.IsNullOrWhiteSpace(original);
+
+// Console.WriteLine("Original: -" + original + "-");
+// Console.WriteLine("ToUpper: -" + s1 + "-");
+// Console.WriteLine("ToLower: -" + s2 + "-");
+// Console.WriteLine("Trim: -" + s3 + "-");
+// Console.WriteLine("IndexOf('bc'): -" + n1);
+// Console.WriteLine("Substring(3): -" + s4 + "-");
+// Console.WriteLine("Substring(3,5): -" + s5 + "-");
+// Console.WriteLine("Replace('a', 'x')): -" + s6 + "-");
+// Console.WriteLine("Replace('abc', 'xy')): -" + s7 + "-");
+// Console.WriteLine("IsNullOrEmpty: -" + b1);
+// Console.WriteLine("IsNullOrWhiteSpace: -" + b2);
+
+// //DATETIME REPRESENTA UM INSTANTE
+// using System.Globalization;
+
+// DateTime d1 = new DateTime(2018, 11, 25);//ano, mes e data
+// DateTime d2 = new DateTime(2018, 11, 25, 20, 45, 3);//ano, mes, data, hora, minuto, segundo
+// DateTime d3 = new DateTime(2018, 11, 25, 20, 45, 3, 500);//ano, mes, data, hora, minuto, segundo, milisegundo
+// Console.WriteLine(d1);
+// Console.WriteLine(d2);
+// Console.WriteLine(d3);
+
+// DateTime d4 = DateTime.Now; //data e hora atual
+// DateTime d5 = DateTime.Today; //data de hoje
+// DateTime d6 = DateTime.UtcNow;//data e horario universal agoras
+// Console.WriteLine();
+// Console.WriteLine(d4);
+// Console.WriteLine(d5);
+// Console.WriteLine(d6);
+
+// //Formata para o formato brasileiro
+// DateTime d7 = DateTime.Parse("2000-08-15");
+// DateTime d8 = DateTime.Parse("2000-08-15 13:05:58");
+// DateTime d9 = DateTime.Parse("15/08/2000");
+// DateTime d10 = DateTime.Parse("15/08/2000 13:05:58");
+// Console.WriteLine();
+// Console.WriteLine(d7);
+// Console.WriteLine(d8);
+// Console.WriteLine(d9);
+// Console.WriteLine(d10);
+
+// //Formata para o formato brasileiro
+// DateTime d11 = DateTime.ParseExact("2000-08-15", "yyyy-MM-dd",CultureInfo.InvariantCulture);
+// DateTime d12 = DateTime.ParseExact("15/08/2000 13:05:58", "dd/MM/yyyy HH:mm:ss",CultureInfo.InvariantCulture);
+// Console.WriteLine();
+// Console.WriteLine(d11);
+// Console.WriteLine(d12);
+
+// //TIME SPAN - REPRESENTA UMA DURACAO NA FORMA DE TICKS(100 NANOSEGUNDOS)
+// TimeSpan t1 = new TimeSpan(0,1,30);
+// TimeSpan t2 = new TimeSpan(900000000L);
+// TimeSpan t3 = new TimeSpan(2,11,21);
+// TimeSpan t4 = new TimeSpan(1,2,11,21);//dia, hora, minuto e segundo
+// TimeSpan t5 = new TimeSpan(1,2,11,21,321);//dia, hora, minuto, segundo, e milisegundo
+
+// TimeSpan t6 = TimeSpan.FromDays(1.5);//um dia e horas
+// TimeSpan t7 = TimeSpan.FromHours(1.5);
+// TimeSpan t8 = TimeSpan.FromMinutes(1.5);
+// TimeSpan t9 = TimeSpan.FromSeconds(1.5);
+// TimeSpan t10 = TimeSpan.FromMilliseconds(1.5);
+// TimeSpan t11 = TimeSpan.FromTicks(900000000);
+
+// Console.WriteLine(t1);
+// Console.WriteLine(t1.Ticks); //devolve em nanossegundos
+
+//OPERACOES COM DATETIME
+// DateTime d = new DateTime(2022, 7, 29, 9, 4, 58, 275);
+// Console.WriteLine(d);
+// Console.WriteLine("1) Date: " + d.Date);
+// Console.WriteLine("2) Day: " + d.Day);
+// Console.WriteLine("3) DayOfWeek: " + d.DayOfWeek);
+// Console.WriteLine("4) DayOfYear: " + d.DayOfYear);
+// Console.WriteLine("5) Hour: " + d.Hour);
+// Console.WriteLine("6) Kind: " + d.Kind);
+// Console.WriteLine("7) Millisecond: " + d.Millisecond);
+// Console.WriteLine("8) Minute: " + d.Minute);
+// Console.WriteLine("9) Month: " + d.Month);
+// Console.WriteLine("10) Second: " + d.Second);
+// // Console.WriteLine("11) Ticks: " + d.Ticks);
+// // Console.WriteLine("12) TimeOfDay: " + d.TimeOfDay);
+// // Console.WriteLine("13) Year: " + d.Year);
+
+// //FORMATACAO 
+// DateTime d = new DateTime(2001, 8, 15, 13, 45, 58);
+// string s1 = d.ToLongDateString();
+// string s2 = d.ToLongTimeString();
+// string s3 = d.ToShortDateString();
+// string s4 = d.ToShortTimeString();
+// string s5 = d.ToString();
+// string s6 = d.ToString("yyyy-MM-dd HH:mm:ss");
+// string s7 = d.ToString("yyyy-MM-dd HH:mm:ss.fff");
+// Console.WriteLine(s1);
+// Console.WriteLine(s2);
+// Console.WriteLine(s3);
+// Console.WriteLine(s4);
+// Console.WriteLine(s5);
+// Console.WriteLine(s6);
+// Console.WriteLine(s7);
+
+//OPERACOES COM TIMESPAN
+
+// TimeSpan t1 = TimeSpan.MaxValue;
+// TimeSpan t2 = TimeSpan.MinValue;
+// TimeSpan t3 = TimeSpan.Zero;
+// Console.WriteLine(t1);
+// Console.WriteLine(t2);
+// Console.WriteLine(t3);
+
+// TimeSpan t = new TimeSpan(2, 3, 5, 7, 11);
+// Console.WriteLine(t);
+// Console.WriteLine("Days: " + t.Days);
+// Console.WriteLine("Hours: " + t.Hours);
+// Console.WriteLine("Minutes: " + t.Minutes);
+// Console.WriteLine("Milliseconds: " + t.Milliseconds);
+// Console.WriteLine("Seconds: " + t.Seconds);
+// Console.WriteLine("Ticks: " + t.Ticks);
+// Console.WriteLine("TotalDays: " + t.TotalDays);
+// Console.WriteLine("TotalHours: " + t.TotalHours);
+// Console.WriteLine("TotalMinutes: " + t.TotalMinutes);
+// Console.WriteLine("TotalSeconds: " + t.TotalSeconds);
+// Console.WriteLine("TotalMilliseconds: " + t.TotalMilliseconds);
+
+// TimeSpan t4 = new TimeSpan(1, 30, 10);
+// TimeSpan t5 = new TimeSpan(0, 10, 5);
+// TimeSpan sum = t1.Add(t2);
+// TimeSpan dif = t1.Subtract(t2);
+// TimeSpan mult = t2.Multiply(2.0);
+// TimeSpan div = t2.Divide(2.0);
+// Console.WriteLine(t4);
+// Console.WriteLine(t5);
+// Console.WriteLine(sum);
+// Console.WriteLine(dif);
+// Console.WriteLine(mult);
+// Console.WriteLine(div);
+
+// DATETIMEKIND(ipo enumerado especial que define três valores possíveis para a
+// localidade da data:
+// • Local [ fuso horário do sistema. Exemplo: São Paulo = GMT -3 ]
+// • Utc [ fuso horário GMT (Greenwich Mean Time) ]
+// • Unspecified
+
+DateTime d1 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Local);
+DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Utc);
+DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58);
+Console.WriteLine("d1: " + d1);
+Console.WriteLine("d1 Kind: " + d1.Kind);
+Console.WriteLine("d1 to Local: " + d1.ToLocalTime());
+Console.WriteLine("d1 to Utc: " + d1.ToUniversalTime());
+Console.WriteLine();
+Console.WriteLine("d2: " + d2);
+Console.WriteLine("d2 Kind: " + d2.Kind);
+Console.WriteLine("d2 to Local: " + d2.ToLocalTime());
+Console.WriteLine("d2 to Utc: " + d2.ToUniversalTime());
+Console.WriteLine();
+Console.WriteLine("d3: " + d3);
+Console.WriteLine("d3 Kind: " + d3.Kind);
+Console.WriteLine("d3 to Local: " + d3.ToLocalTime());
+Console.WriteLine("d3 to Utc: " + d3.ToUniversalTime());
+
+DateTime d4 = DateTime.Parse("2000-08-15 13:05:58");
+DateTime d5 = DateTime.Parse("2000-08-15T13:05:58Z"); // cria local DateTime
+Console.WriteLine("d1: " + d4);
+Console.WriteLine("d1 Kind: " + d4.Kind);
+Console.WriteLine("d1 to Local: " + d4.ToLocalTime());
+Console.WriteLine("d1 to Utc: " + d4.ToUniversalTime());
+Console.WriteLine();
+Console.WriteLine("d2: " + d5);
+Console.WriteLine("d2 Kind: " + d5.Kind);
+Console.WriteLine("d2 to Local: " + d5.ToLocalTime());
+Console.WriteLine("d2 to Utc: " + d5.ToUniversalTime());
+Console.WriteLine();
+Console.WriteLine(d5.ToString("yyyy-MM-ddTHH:mm:ssZ")); // cuidado!
+Console.WriteLine(d5.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
